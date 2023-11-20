c21c6223b10b0a7263987b004ed6afceb72c55d7
Shalitha <shalithamorapitiya@gmail.com>
2023-11-20 23:08:49 +0530

    Initial Commit

---
+++ group_divider_app/android/app/src/main/kotlin/com/example/group_divider_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.group_divider_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ group_divider_app/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:group_divider_app/app.dart';
+
+void main() {
+  runApp(const GroupDividerApp());
+}
+
